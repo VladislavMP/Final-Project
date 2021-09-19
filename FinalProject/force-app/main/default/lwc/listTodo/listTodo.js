8f3bfc6c1e8fe1e47464ef6456928d43d35fc195
VeranikaKaraliova <nika-11@mail.ru>
2021-09-19 22:38:40 +0300

    refactor: rename vars for better readability and update the ui

--- FinalProject/force-app/main/default/lwc/listTodo/listTodo.js
+++ FinalProject/force-app/main/default/lwc/listTodo/listTodo.js
@@ -10,128 +10,128 @@ import STATUS_SUB_FIELD from '@salesforce/schema/Sub_ToDo__c.Status__c'
 import NAME_TODO_FIELD from '@salesforce/schema/ToDo__c.Name';
 
 export default class ListTodo extends LightningElement {
-  strName;
-  recordTodoId;
-  recordTodoName;
-  
-  @wire(getTodoList)
-  listTodo
+    strName;
+    recordTodoId;
+    recordTodoName;
 
-  @api
-  recordtype;
+    @wire(getTodoList)
+    listTodo
 
-  @api recordId;
-  @api objectApiNameTodo = TODO_OBJECT;
-  @api objectApiName = SUB_TODO_OBJECT;
-  fieldsTodo = [NAME_TODO_FIELD];
-  fields = [NAME_SUB_FIELD, STATUS_SUB_FIELD];
+    @api
+    recordtype;
 
-  modalWindowAdd = false
-  modalWindowEditTodo = false;
-  modalWindowEdit = false;
+    @api recordId;
+    @api objectApiNameTodo = TODO_OBJECT;
+    @api objectApiName = SUB_TODO_OBJECT;
+    fieldsTodo = [NAME_TODO_FIELD];
+    fields = [NAME_SUB_FIELD, STATUS_SUB_FIELD];
 
-  get devTodos(){
-    return this.listTodo.data.filter(todo => todo.RecordType.DeveloperName === this.recordtype);
-  }
+    modalWindowAdd = false
+    modalWindowEditTodo = false;
+    modalWindowEdit = false;
 
-  openAccordion(event){
-    let el = event.target.closest('.slds-accordion__section')
-    let elBtn = event.target.closest('.todo-name')
-    if(!el.classList.contains('slds-is-open')){
-      elBtn.childNodes[1].classList.add('btn-arrow-transform');
-      el.classList.add('slds-is-open');
-    }else{
-      elBtn.childNodes[1].classList.remove('btn-arrow-transform')
-      el.classList.remove('slds-is-open');
+    get devTodos() {
+        return this.listTodo.data.filter(todo => todo.RecordType.DeveloperName === this.recordtype);
     }
-  }
 
-  closeWindowAdd(){
-    this.modalWindowAdd = false;
-  }
+    openAccordion(event) {
+        let accordion = event.target.closest('.slds-accordion__section')
+        let btn = event.target.closest('.todo-name')
+        if (!accordion.classList.contains('slds-is-open')) {
+            btn.childNodes[1].classList.add('btn-arrow-transform');
+            accordion.classList.add('slds-is-open');
+        } else {
+            btn.childNodes[1].classList.remove('btn-arrow-transform')
+            accordion.classList.remove('slds-is-open');
+        }
+    }
 
-  closeWindowEditTodo(){
-    this.modalWindowEditTodo = false;
-  }
+    closeWindowAdd() {
+        this.modalWindowAdd = false;
+    }
 
-  closeWindowEdit(){
-    this.modalWindowEdit = false;
-  }
+    closeWindowEditTodo() {
+        this.modalWindowEditTodo = false;
+    }
 
-  nameChangedHandler(event){
-    this.strName = event.target.value;
-  }
+    closeWindowEdit() {
+        this.modalWindowEdit = false;
+    }
 
-  createSubTodo(){
-    let fields = {};
-    fields[NAME_SUB_FIELD.fieldApiName] = this.strName;
-    fields['ToDo__c'] = this.recordTodoId;
-    let objRecordInput = {apiName : SUB_TODO_OBJECT.objectApiName, fields};
-    this.modalWindowAdd = false;
-    createRecord(objRecordInput).then(response => {
-      this.dispatchEvent(
-        new ShowToastEvent({
-        title: "Record created",
-        variant: "success"
-        })
-      )
-      refreshApex(this.listTodo);
-    }).catch((error) => {
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: "ERROR",
-          message: "Record not created",
-          variant: "error"
-        })
-      )
-    });
-  }
+    nameChangedHandler(event) {
+        this.strName = event.target.value;
+    }
 
-  editRecordTodo(event){
-    this.recordId = event.target.dataset.recordid;
-    this.modalWindowEditTodo = true;
-  }
+    createSubTodo() {
+        let fields = {};
+        fields[NAME_SUB_FIELD.fieldApiName] = this.strName;
+        fields['ToDo__c'] = this.recordTodoId;
+        let objRecordInput = { apiName: SUB_TODO_OBJECT.objectApiName, fields };
+        this.modalWindowAdd = false;
+        createRecord(objRecordInput).then(response => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "Record created",
+                    variant: "success"
+                })
+            )
+            refreshApex(this.listTodo);
+        }).catch((error) => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "ERROR",
+                    message: "Record not created",
+                    variant: "error"
+                })
+            )
+        });
+    }
 
-  editRecord(event){
-    this.recordId = event.target.dataset.recordid;
-    this.modalWindowEdit = true;
-  }
+    editRecordTodo(event) {
+        this.recordId = event.target.dataset.recordid;
+        this.modalWindowEditTodo = true;
+    }
 
-  deleteRecord(event){
-    let recordId = event.target.dataset.recordid;
-    deleteRecord(recordId)
-      .then(()=>{
-        this.dispatchEvent(
-          new ShowToastEvent({
-          title: "The record deleted",
-          variant: "success"
-          })
-        )
-        refreshApex(this.listTodo); 
-      })
-      .catch((error)=>{
-        this.dispatchEvent(
-          new ShowToastEvent({
-            title: "ERROR",
-            message: "Record not deleted",
-            variant: "error"
-          })
-        )
-      })
-  }
+    editRecord(event) {
+        this.recordId = event.target.dataset.recordid;
+        this.modalWindowEdit = true;
+    }
 
-  addNewSubTodo(event){
-    this.modalWindowAdd = true
-    this.recordTodoId = event.target.dataset.keyid;
-    this.recordTodoName = event.target.dataset.keyname;
-  }
+    deleteRecord(event) {
+        let recordId = event.target.dataset.recordid;
+        deleteRecord(recordId)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "The record deleted",
+                        variant: "success"
+                    })
+                )
+                refreshApex(this.listTodo);
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "ERROR",
+                        message: "Record not deleted",
+                        variant: "error"
+                    })
+                )
+            })
+    }
 
-  handleSuccessEdit() {
-    refreshApex(this.listTodo);
-    const toastEvent = new ShowToastEvent({
-      title: "The record edited",
-      variant: "success"
-    });
-    this.dispatchEvent(toastEvent);
-  }
+    addNewSubTodo(event) {
+        this.modalWindowAdd = true
+        this.recordTodoId = event.target.dataset.keyid;
+        this.recordTodoName = event.target.dataset.keyname;
+    }
+
+    handleSuccessEdit() {
+        refreshApex(this.listTodo);
+        const toastEvent = new ShowToastEvent({
+            title: "The record edited",
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
 }
